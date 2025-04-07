0e0602d6097f9e2555b126b8ab604ac2de6e094a
Bryce Carter <carterbryce54@gmail.com>
2025-04-07 16:44:55 -0600

    Movie data provided with primary genre

---
+++ backend/Intex1_15.API/Data/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intex1_15.API.Data
+{
+    public class Movie
+    {
+        [Key]
+        public int show_id { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public string cast { get; set; }
+        public string country { get; set; }
+        public int release_year { get; set; }
+        public string rating { get; set; }
+        public string duration { get; set; }
+        public string description { get; set; }
+    }
+}
