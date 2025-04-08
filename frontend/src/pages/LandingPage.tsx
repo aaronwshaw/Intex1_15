38d73ad621734d139ed6f6ba7654c366f953daa5
mjpalm13 <mjpalm2006@gmail.com>
2025-04-08 14:11:21 -0600

    css changes

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,14 @@
+import Navbar from '../components/Navbar/Navbar'; // adjust path if needed
+
 function LandingPage() {
-  return <></>;
+  console.log('Rendering CategorySlider');
+
+  return (
+    <div className="overflow-x-hidden" style={{ paddingTop: '5rem' }}>
+      <Navbar />
+    </div>
+  
+  );
 }
 
 export default LandingPage;
