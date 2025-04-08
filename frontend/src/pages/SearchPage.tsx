852106cd55d41d2963db716b93378507da0398a6
LiterallyMe-kun <sknoah2002@gmail.com>
2025-04-08 10:30:50 -0600

    beginning search page

---
+++ frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,11 @@
+import AuthorizeView from '../components/AuthorizeView';
+
+function SearchPage() {
+  return (
+    <AuthorizeView>
+      <h1>Search Page</h1>
+    </AuthorizeView>
+  );
+}
+
+export default SearchPage;
