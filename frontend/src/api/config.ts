72df99c39ed7e36a769244b8c64893aca325eae6
aaronwshaw <ashaw32@byu.edu>
2025-04-10 11:19:24 -0600

    production branch

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -2,9 +2,9 @@
 
 // src/api/config.ts
 const local = 'https://localhost:5000';
-// const production =
-//   'https://intex1-15-backend-d9ehehbcgrajf7fk.eastus-01.azurewebsites.net';
+const production =
+  'https://intex1-15-backend-d9ehehbcgrajf7fk.eastus-01.azurewebsites.net';
 
-export const API_url = local;
+export const API_url = production;
 //production;
 //import.meta.env.MODE === 'development' ? local : production;
